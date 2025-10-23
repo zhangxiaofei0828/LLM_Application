e1788315bb35566c9bc0290c7c2bbd31804210e9
zhangxiaofei0828 <2398220267@qq.com>
2025-10-23 17:02:01 +0800

    置空

--- langchain-fastapi-chatbot/src/core/config.py
+++
@@ -1,9 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-class Config:
-    CSV_FILE_PATH = os.getenv("CSV_FILE_PATH", "path/to/your/private/documents.csv")
-    PORT = int(os.getenv("PORT", 8000))
-    HOST = os.getenv("HOST", "0.0.0.0")
